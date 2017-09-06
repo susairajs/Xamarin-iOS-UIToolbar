98789533e4ae9f08d43d2e8af4eaf4a7aa8a6c7e
DelpinSusaiRaj <susairajs@outlook.com>
2017-09-06 15:49:17 +0530

    All files Added

---
+++ XamariniOSUIToolbar/ViewController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+using UIKit;
+
+namespace XamariniOSUIToolbar
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		partial void UIBarButtonItem69_Activated(UIBarButtonItem sender)
+		{
+			UIAlertView alert = new UIAlertView("Nofication", "Camera Clicked", null, "OK");
+			alert.Show();
+		}
+
+		partial void UIBarButtonItem45_Activated(UIBarButtonItem sender)
+		{
+			UIAlertView alert = new UIAlertView("Nofication", "Item Deleted", null, "OK");
+			alert.Show();
+		}
+
+		partial void UIBarButtonItem70_Activated(UIBarButtonItem sender)
+		{
+			UIAlertView alert = new UIAlertView("Nofication", "Item Saved", null, "OK");
+			alert.Show();
+		}
+
+		partial void UIBarButtonItem66_Activated(UIBarButtonItem sender)
+		{
+			UIAlertView alert = new UIAlertView("Nofication", "Added to bookmark", null, "OK");
+			alert.Show();
+		}
+
+		partial void UIBarButtonItem65_Activated(UIBarButtonItem sender)
+		{
+			UIAlertView alert = new UIAlertView("Nofication", "Add a New items", null, "OK");
+			alert.Show();
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
---
+++ XamariniOSUIToolbar/ViewController.designer.cs
@@ -0,0 +1,40 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace XamariniOSUIToolbar
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Action ("UIBarButtonItem45_Activated:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void UIBarButtonItem45_Activated (UIKit.UIBarButtonItem sender);
+
+        [Action ("UIBarButtonItem65_Activated:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void UIBarButtonItem65_Activated (UIKit.UIBarButtonItem sender);
+
+        [Action ("UIBarButtonItem66_Activated:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void UIBarButtonItem66_Activated (UIKit.UIBarButtonItem sender);
+
+        [Action ("UIBarButtonItem69_Activated:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void UIBarButtonItem69_Activated (UIKit.UIBarButtonItem sender);
+
+        [Action ("UIBarButtonItem70_Activated:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void UIBarButtonItem70_Activated (UIKit.UIBarButtonItem sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+        }
+    }
+}
